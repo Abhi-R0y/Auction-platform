e715296c7ffefd7926ad1b381401249549b40858
Abhi-R0y <araj87812@gmail.com>
2025-04-05 13:52:25 +0530

    Bug Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import AuctionsPage from "./pages/AuctionsPage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
